0e4fadc08cdf8cacfa4c4dcf5ba0086677441226
Johnny Silverhand <hello@bnbpad.ai>
2025-12-05 16:41:07 +0400

    Add AgentLaunchpad contract and integrate with AgentNFT
    - Introduced AgentLaunchpad contract with initialization and agent creation functionality.
    - Updated CounterScript to instantiate AgentLaunchpad and initialize it with AgentNFT.
    - Changed launchpad variable in Agent contract to be immutable for enhanced security.
    - Refactored sendRequest function in AgentLaunchpad to accept agent address as a parameter.

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
 import {AgentNFT} from "../src/AgentNFT.sol";
+import {AgentLaunchpad} from "../src/AgentLaunchpad.sol";
 
 contract CounterScript is Script {
     AgentNFT public agentNFT;
+    AgentLaunchpad public agentLaunchpad;
 
     function setUp() public {}
 
@@ -14,6 +16,10 @@ contract CounterScript is Script {
 
         // counter = new Counter();
         agentNFT = new AgentNFT();
+        agentLaunchpad = new AgentLaunchpad();
+
+        agentNFT.initialize("Phunks", "Punks", address(this));
+        agentLaunchpad.initialize(address(agentNFT));
 
         vm.stopBroadcast();
     }
